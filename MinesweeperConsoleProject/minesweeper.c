7ae8b3e2197585ca4c8e6ee4360789905b57bbbb
Ash <afalzon12@gmail.com>
2023-04-13 15:20:33 +0200

    Update minesweeper.c

--- MinesweeperConsoleProject/minesweeper.c
+++ MinesweeperConsoleProject/minesweeper.c
@@ -1,52 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <time.h>
 
-#define BOARD_SIZE 10
-#define NUM_MINES 10
+#define MAX_ROWS 20
+#define MAX_COLS 20
+#define MINE_PROBABILITY 0.15
 
-int board[BOARD_SIZE][BOARD_SIZE];
-int revealed[BOARD_SIZE][BOARD_SIZE];
-int num_mines_remaining = NUM_MINES;
+typedef struct {
+    char symbol;
+    int adjacent_mines;
+    bool has_mine;
+    bool revealed;
+    bool marked;
+} Cell;
 
-int is_valid(int row, int col) {
-    return (row >= 0 && row < BOARD_SIZE && col >= 0 && col < BOARD_SIZE);
-}
+Cell board[MAX_ROWS][MAX_COLS];
+bool game_over = false;
+int rows, cols, num_mines;
 
-int is_mine(int row, int col) {
-    return board[row][col] == -1;
-}
-
-void initialize_board(char board[][MAX_COLS], int rows, int cols) {
-    int i, j;
-    
-    // initialize all cells to 'X'
-    for (i = 0; i < rows; i++) {
-        for (j = 0; j < cols; j++) {
-            board[i][j] = 'X';
+void initialize_board() {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            board[i][j].symbol = 'X';
+            board[i][j].adjacent_mines = 0;
+            board[i][j].has_mine = false;
+            board[i][j].revealed = false;
+            board[i][j].marked = false;
         }
     }
 }
 
-
 void print_board() {
-    printf("Mines remaining: %d\n", num_mines_remaining);
-    printf("   ");
-    for (int i = 0; i < BOARD_SIZE; i++) {
+    printf("\n    ");
+    for (int i = 0; i < cols; i++) {
         printf("%d ", i);
     }
-    printf("\n");
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        printf("%2d ", i);
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            if (revealed[i][j]) {
-                if (is_mine(i, j)) {
-                    printf("* ");
-                } else {
-                    printf("%d ", board[i][j]);
-                }
+    printf("\n\n");
+    for (int i = 0; i < rows; i++) {
+        printf("%d   ", i);
+        for (int j = 0; j < cols; j++) {
+            if (board[i][j].marked) {
+                printf("M ");
+            } else if (board[i][j].revealed) {
+                printf("%d ", board[i][j].adjacent_mines);
             } else {
-                printf(". ");
+                printf("%c ", board[i][j].symbol);
             }
         }
         printf("\n");
@@ -54,70 +53,86 @@ void print_board() {
     printf("\n");
 }
 
+bool is_valid_cell(int row, int col) {
+    return row >= 0 && row < rows && col >= 0 && col < cols;
+}
+
 void place_mines() {
-    for (int i = 0; i < NUM_MINES; i++) {
-        int row = rand() % BOARD_SIZE;
-        int col = rand() % BOARD_SIZE;
-        if (board[row][col] == -1) {
-            i--;
-        } else {
-            board[row][col] = -1;
-            for (int j = row - 1; j <= row + 1; j++) {
-                for (int k = col - 1; k <= col + 1; k++) {
-                    if (is_valid(j, k) && board[j][k] != -1) {
-                        board[j][k]++;
-                    }
+    int num_cells = rows * cols;
+    num_mines = num_cells * MINE_PROBABILITY;
+    for (int i = 0; i < num_mines; i++) {
+        int row, col;
+        do {
+            row = rand() % rows;
+            col = rand() % cols;
+        } while (board[row][col].has_mine);
+        board[row][col].has_mine = true;
+        for (int j = row - 1; j <= row + 1; j++) {
+            for (int k = col - 1; k <= col + 1; k++) {
+                if (is_valid_cell(j, k)) {
+                    board[j][k].adjacent_mines++;
                 }
             }
         }
     }
 }
 
-void reveal(int row, int col) {
-    if (revealed[row][col]) {
+void reveal_cell(int row, int col) {
+    if (!is_valid_cell(row, col) || board[row][col].revealed) {
         return;
     }
-    revealed[row][col] = 1;
-    if (is_mine(row, col)) {
-        printf("Game over!\n");
-        print_board();
-        exit(0);
+    board[row][col].revealed = true;
+    if (board[row][col].has_mine) {
+        game_over = true;
+        return;
     }
-    if (board[row][col] == 0) {
-        for (int i = row - 1; i <= row + 1; i++) {
-            for (int j = col - 1; j <= col + 1; j++) {
-                if (is_valid(i, j)) {
-                    reveal(i, j);
-                }
+    if (board[row][col].adjacent_mines == 0) {
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                reveal_cell(row + i, col + j);
             }
         }
     }
 }
 
-void play_game() {
-    int row, col;
-    while (num_mines_remaining > 0) {
-        print_board();
-        printf("Enter row and column to reveal: ");
-        scanf("%d %d", &row, &col);
-        if (!is_valid(row, col)) {
-            printf("Invalid input, try again.\n");
-        } else if (revealed[row][col]) {
-            printf("That square has already been revealed, try again.\n");
-        } else {
-            reveal(row, col);
-            if (num_mines_remaining == 0) {
-                printf("You win!\n");
-                print_board();
-            }
-        }
+void mark_cell(int row, int col) {
+    if (!is_valid_cell(row, col)) {
+        return;
     }
+    board[row][col].marked = !board[row][col].marked;
 }
 
 int main() {
     srand(time(NULL));
+    printf("Enter number of rows (maximum %d): ", MAX_ROWS);
+    scanf("%d", &rows);
+    printf("Enter number of columns (maximum %d): ", MAX_COLS);
+    scanf("%d", &cols);
     initialize_board();
     place_mines();
-    play_game();
+    int remaining_cells = rows * cols - num_mines;
+    while (!game_over && remaining_cells > 0) {
+        print_board();
+        int row, col;
+        printf("Enter row and column (separated by a space) or 'M' to mark/unmark a cell: ");
+        if (scanf("%d %d", &row, &col) == 2) {
+            if (board[row][col].has_mine) {
+                game_over = true;
+            } else {
+                reveal_cell(row, col);
+                remaining_cells--;
+            }
+        } else {
+            char mark;
+            scanf(" %c", &mark);
+            mark_cell(row, col);
+        }
+    }
+    print_board();
+    if (game_over) {
+        printf("Game over! You hit a mine.\n");
+    } else {
+        printf("Congratulations, you won!\n");
+    }
     return 0;
 }
